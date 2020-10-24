661ca69ed8f35e05cc68b43aba49fcecabfdbed0
Annie Bedford <abedford@gmail.com>
2020-10-24 10:32:15 +0100

    Added functionality to display todo items and to add new task

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+
+from todo_app.data.session_items import add_item, get_items
+from flask import Flask, render_template, request
 
 from todo_app.flask_config import Config
 
@@ -8,8 +10,20 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+   items = get_items()
+   return render_template('index.html', title='To Do App', items=items)
 
+
+@app.route('/items/add', methods = ['POST', 'GET'])
+def add_items():
+   if request.method == 'POST':
+      form_data = request.form
+      task = form_data["title"]
+      add_item(task)
+
+      return index()
+        
+   return index()
 
 if __name__ == '__main__':
     app.run()
