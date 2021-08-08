69c4ef0ea58a25622dd2070201b7dcdc732eb49f
Annie Bedford <abedford@gmail.com>
2021-08-08 08:20:27 +0100

    Added changes to get the redirect working and trying to get the token from the access code

--- todo_app/app.py
+++ todo_app/app.py
@@ -6,6 +6,9 @@ from flask_login import login_required
 from flask_login import LoginManager
 import os
 import requests
+import json
+from oauthlib.oauth2 import WebApplicationClient
+
 
 def create_app(db_name = ""):
    app = Flask(__name__)
@@ -20,12 +23,11 @@ def create_app(db_name = ""):
 
    mongo_client = ToDoMongoClient(mongo_user, mongo_pwd, mongo_srv, db_name, mongo_connection)
 
-   login_manager = LoginManager() 
-   login_manager.init_app(app)
+   
 
    #  All the routes and setup code etc
-   @login_required
    @app.route('/')
+   @login_required
    def index():
       show_all = request.args.get('show_all')
       
@@ -89,23 +91,60 @@ def create_app(db_name = ""):
          
       return redirect('/')
 
+   @app.route('/login/callback', methods = ['GET'])
+   def login():
+      print("Processing the login route")
+      
+      if request.method == 'GET':
+         oauth_client_id = os.getenv('OAUTH_CLIENT_ID')
+         oauth_secret_id = os.getenv('OAUTH_SECRET_ID')
+
+         client = WebApplicationClient(oauth_client_id)
+         # Prepare and send a request to get tokens
+         token_url, headers, body = client.prepare_token_request(
+            "https://github.com/login/oauth/access_token",
+            authorization_response=request.url,
+            redirect_url=request.base_url,
+            code="" # get the code somehow
+         )
+         token_response = requests.post(
+            token_url,
+            headers=headers,
+            data=body,
+            auth=(oauth_client_id, oauth_secret_id)
+         )
+
+         # Parse the tokens!
+         
+         token = client.parse_request_body_response(json.dumps(token_response.json()))
+
+      return redirect(request.url)
+
+
 
    if __name__ == '__main__':
       app.run()
 
-
-   
+   login_manager = LoginManager() 
+   login_manager.init_app(app)
+   oauth_client_id = os.getenv('OAUTH_CLIENT_ID')
+   oauth_secret_id = os.getenv('OAUTH_SECRET_ID')
+   client = WebApplicationClient(oauth_client_id)
 
  
    @login_manager.unauthorized_handler 
    def unauthenticated(): 
       # need to call GET https://github.com/login/oauth/authorize with clientid and redirect_uri
-      authorize_url = 'https://github.com/login/oauth/authorize?client_id=640c2ac9d976df608c2b&redirect_uri=http://127.0.0.1:5000/login/'
+      authorize_endpoint = 'https://github.com/login/oauth/authorize'
       
-      print("Trying to redirect to github authorization")
+      authorize_url = client.prepare_request_uri(
+        authorize_endpoint, "http://127.0.0.1:5000/login/callback")
       return redirect(authorize_url)
+
+
       
-      # Then we will post the access_toekn back to github
+      # returns http://127.0.0.1:5000/login/callback/?code=ee365f5d6af9dff0e7b9
+      # Then we will post the access_token back to github
       # POST https://github.com/login/oauth/access_token
       # client_id
       # client secret
